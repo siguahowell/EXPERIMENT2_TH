ad94101ca713093f87d93107e211f9b4e43a3951
siguahowell <47727452+siguahowell@users.noreply.github.com>
2019-05-04 20:07:30 +0800

    Update #2.cpp

--- #2.cpp
+++ #2.cpp
@@ -5,24 +5,23 @@ using namespace std;
 
 int main () 
 {
-	double gallons,waterbill, total_waterbill, meters,last_month,during_month ; 
+	double gallons,waterbill, total_waterbill, cubicmeters,last_month,during_month ; 
 	const int water_demand=35, late_charge=20;
 	
 	cout<<"Enter the amount of water bill previous month:";
 	cin >>last_month;
-	cout<<"enter the of water bill during this month:";
-	cin>>during_month;
-	cout <<"Enter the gallons of water used in meteric units:";
-	cin>>meters; 
-	waterbill=last_month+during_month;
+	cout <<"Enter the gallons of water used :";
+	cin>>gallons; 
+	cubicmeters=gallons*0.00378541;
+	waterbill=last_month;
 	//if statements
 		if (last_month > 0){
 		
-		total_waterbill = waterbill+water_demand+(1.10*meters)+late_charge;
+		total_waterbill = waterbill+water_demand+(1.10*cubicmeters)+late_charge;
 		cout << "your water bill with additional fine is:" << total_waterbill<<"php";
 		}else if (last_month == 0){
 		
-		total_waterbill=during_month+water_demand+(1.10*meters);
+		total_waterbill=water_demand+(1.10*cubicmeters);
 		cout<<"your water bill is:"<<total_waterbill<<"php";
 		}else 
 		cout<<"invalid input just use positive numbers";
@@ -33,4 +32,3 @@ int main ()
 
 
 	
-
