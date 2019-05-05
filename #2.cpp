543e0540443084b39aeaa92560319c3788b9abb0
siguahowell <47727452+siguahowell@users.noreply.github.com>
2019-05-05 17:53:05 +0800

    Update #2.cpp

--- #2.cpp
+++ #2.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main () 
 {
-	double gallons,waterbill, total_waterbill, meters,last_month,during_month ; 
+	double gallons,waterbill, total_waterbill, meters,last_month,during_month,total_lastmonth ; 
 	const int water_demand=35, late_charge=20;
 	
-	cout<<"Enter the amount of water bill previous month:";
+	cout<<"Enter the gallons used last month:";
 	cin >>last_month;
-	cout <<"Enter the gallons of water used:";
+	cout <<"Enter the gallons of water used during the month:";
 	cin>>gallons; 
-	waterbill=last_month;
+	total_lastmonth=last_month*1.10;
 	//if statements
 		if (last_month > 0){
 		
-		total_waterbill = last_month+water_demand+(1.10*gallons)+late_charge;
+		total_waterbill = total_lastmonth+water_demand+(1.10*gallons)+late_charge;
 		cout << "your water bill with additional fine is:" << total_waterbill<<"php";
 		}else if (last_month == 0){
 		
@@ -28,9 +28,3 @@ int main ()
 	return 0;		
 
 }
-
-
-	
-
-
-	
